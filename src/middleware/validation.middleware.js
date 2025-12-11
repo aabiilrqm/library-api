23b7badea2e026d5ea99adca6f25a11de32afc4a
aabiilrqm <abilarqam0@gmail.com>
2025-12-12 05:15:27 +0800

    Membuat validator input dan validation middleware

---
+++ src/middleware/validation.middleware.js
@@ -0,0 +1,13 @@
+const { error } = require("../utils/response");
+
+module.exports = (schema) => {
+  return (req, res, next) => {
+    const { error: validationError } = schema.validate(req.body);
+
+    if (validationError) {
+      return error(res, validationError.details[0].message, 400);
+    }
+
+    next();
+  };
+};
