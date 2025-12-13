858fdff16409a3618ad4ded291c4105fda77122d
aabiilrqm <abilarqam0@gmail.com>
2025-12-13 13:26:03 +0800

    Verifikasi signature token menggunakan secret key

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -12,7 +12,25 @@ const generateRefreshToken = (payload) => {
   });
 };
 
+const verifyAccessToken = (token) => {
+  try {
+    return jwt.verify(token, process.env.JWT_SECRET); // Verifikasi token dengan secret
+  } catch (error) {
+    throw new Error("Invalid or expired access token"); // Custom error message
+  }
+};
+
+const verifyRefreshToken = (token) => {
+  try {
+    return jwt.verify(token, process.env.JWT_REFRESH_SECRET); // Verifikasi dengan refresh secret
+  } catch (error) {
+    throw new Error("Invalid or expired refresh token");
+  }
+};
+
 module.exports = {
   generateAccessToken,
   generateRefreshToken,
+  verifyAccessToken,
+  verifyRefreshToken
 };
