f0e76bb6349e8a501567d787e41909d018c8090c
aabiilrqm <abilarqam0@gmail.com>
2025-12-14 19:09:19 +0800

    Mengubah port

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
-// src/server.js
 require("dotenv").config();
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
+const HOST = process.env.HOST || "0.0.0.0"; // Tambah ini!
 
-app.listen(PORT, () => {
+app.listen(PORT, HOST, () => {
+  // Gunakan HOST parameter
   console.log(
-    `Server running on port ${PORT} (NODE_ENV=${
+    `Server running on http://${HOST}:${PORT} (NODE_ENV=${
       process.env.NODE_ENV || "development"
     })`
   );
